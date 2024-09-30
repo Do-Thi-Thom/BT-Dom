1b5935e1c149c4ecce3366cc1a235eb5d926002c
Do-Thi-Thom <dothithom.5894.gkfxprime@gmail.com>
2024-09-30 14:16:33 +0700

    Add files via upload

---
+++ bt_Dom.js
@@ -0,0 +1,5 @@
+// BT_01-------------------------------------------------------
+// Không viết thêm gì vào thẻ ngoại trừ nhúng javascript trong file html, viết chương trình hiển thị danh sách lên trang web như sau
+// list item number 1
+// bài giải bên html
+a;
